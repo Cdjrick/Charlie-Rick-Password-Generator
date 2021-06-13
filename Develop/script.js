0fe608a86a9dce6eaec3f6fb9456f8a57d538d6c
Charlie Rick <rick0048@algonquinlive.com>
2021-06-13 19:05:35 -0400

    updated the upper and lower case randomizer

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,10 @@
 // Assignment code here
 
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-/* There is probably a more efficient way for doing upper and lower letters, but this was the only way i could get to work - 
-- See Lower*/
-let upperAlphabet = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+/* This was my first working attempt at upper and lower letter randomizer - 
+- See Lower
+let upperAlphabet = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+*/
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 let specialChars = [' ', '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '_', '{', '|', '}', '"']
@@ -13,7 +14,11 @@ function generatePassword(length, upperAndLower, passNumbers, passSpecialChars) 
   let passCriteria = [alphabet]
 
   if(upperAndLower) {
-    passCriteria.push(upperAlphabet)
+    /* My 3rd working attempt at upper and lower letter randomizer*/
+    let upperCase = alphabet.map(char => {
+      return char.toUpperCase()
+    })
+    passCriteria.push(upperCase)
   }
 
   if(passNumbers) {
@@ -33,7 +38,7 @@ function generatePassword(length, upperAndLower, passNumbers, passSpecialChars) 
     let chosenChar = Math.floor(Math.random() * criteria.length)
     let char = criteria[chosenChar]
 
-/* This was my attempt on doing an efficient upper and lower letter randomizer
+/* This was my 2nd attempt on doing an efficient upper and lower letter randomizer
 
     if(upperAndLower) {
       let toUpperCase = Math.floor(Math.random() * 1) == 1
